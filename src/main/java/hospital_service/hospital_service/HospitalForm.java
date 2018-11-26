2b732e2b58fb7846f6fc4b682446bcae471b1a9e
Mariusz <you@example.com>
2018-11-26 23:34:02 +0100

    Add Hospital form

---
+++ src/main/java/hospital_service/hospital_service/HospitalForm.java
@@ -0,0 +1,17 @@
+package hospital_service.hospital_service;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class HospitalForm {
+
+    private String name;
+    private String country;
+    private String town;
+    private String street;
+}
