d0c4027183ff4c87dfa2ca49cc9209f6f6683c10
Mariusz <you@example.com>
2018-11-27 01:48:09 +0100

    Add hospital controlle

---
+++ src/main/java/hospital_service/hospital_service/HospitalController.java
@@ -0,0 +1,10 @@
+package hospital_service.hospital_service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hospitalList")
+public class HospitalController {
+}
