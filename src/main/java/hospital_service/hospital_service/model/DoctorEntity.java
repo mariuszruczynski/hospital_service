1f050a4b94dc25db3a005b6680bcf4bf5fab9bd9
Mariusz <you@example.com>
2018-11-27 11:52:05 +0100

    Add doctror entity

--- src/main/java/hospital_service/hospital_service/model/DoctorEntity.java
+++ src/main/java/hospital_service/hospital_service/model/DoctorEntity.java
@@ -1,4 +1,27 @@
 package hospital_service.hospital_service.model;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "doctors")
 public class DoctorEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column (name = "name")
+    private String name;
+    @Column (name = "surname")
+    private String surname;
+    @Column(name = "licence_number", unique = true, nullable = false)
+    private String licenceNumber;
+    @Column (name = "nationality")
+    private String nationality;
+
 }
