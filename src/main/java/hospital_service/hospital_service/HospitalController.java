86af5275e3feb60c185aa2e920100bdc4951dbdb
Mariusz <you@example.com>
2018-11-27 08:07:05 +0100

    Add hospital entity service

--- src/main/java/hospital_service/hospital_service/HospitalController.java
+++ src/main/java/hospital_service/hospital_service/HospitalController.java
@@ -1,10 +1,24 @@
 package hospital_service.hospital_service;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/hospitalList")
 public class HospitalController {
+
+    private final HospitalEntityService hospitalEntityService;
+
+    public HospitalController(HospitalEntityService hospitalEntityService) {
+        this.hospitalEntityService = hospitalEntityService;
+    }
+
+    @GetMapping(path = {"/", "/all"})
+    public String findAll(Model model) {
+        model.addAttribute("hospitals", hospitalEntityService.findAll());
+        return "hospitalList";
+    }
 }
---
+++ src/main/java/hospital_service/hospital_service/HospitalEntityService.java
@@ -0,0 +1,15 @@
+package hospital_service.hospital_service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class HospitalEntityService {
+
+
+    public Object findAll() {
+        return 0;
+    }
+}
